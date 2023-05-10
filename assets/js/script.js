ce98291c15801d2fe7fe1b3a1ae4f923725c0cf7
Jess <jess.m.jerez@gmail.com>
2023-05-10 17:38:29 -0600

    added html and API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,37 @@
-var search = document.querySelector(".search");
-var searchBar = document.querySelector(".searchBar");
-var searchBtn = document.querySelector(".searchBtn");
-var searchHistory = document.querySelector(".searchHistory");
-var currentForecast = document.querySelector(".currentForecast");
-var currentCard = document.querySelector(".currentCard");
-var futureForecast = document.querySelector(".futureForecast");
-var forecastCard = document.querySelector(".forecastCard");
-var key = '6ed725886cc218c6ea1bf1fcf160a3de';
+let APIKey = '6ed725886cc218c6ea1bf1fcf160a3de';
 
-function fetchCurrentWeather(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`)
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data);
-        var currentTemp = document.createElement("h4")
-        currentTemp.innerHTML = `Temp: ${data.main.temp}  &#176F`
-        currentCard.append(currentTemp)
-    });
-};
 
-searchBtn.addEventListener("click", function() {
-    var city = searchBar.value;
-    localStorage.setItem("city", JSON.stringify(city));
-    fetchCurrentWeather(city);
-});
+
+
+
+
+
+// var search = document.querySelector(".search");
+// var searchBar = document.querySelector(".searchBar");
+// var searchBtn = document.querySelector(".searchBtn");
+// var searchHistory = document.querySelector(".searchHistory");
+// var currentForecast = document.querySelector(".currentForecast");
+// var currentCard = document.querySelector(".currentCard");
+// var futureForecast = document.querySelector(".futureForecast");
+// var forecastCard = document.querySelector(".forecastCard");
+// var key = '6ed725886cc218c6ea1bf1fcf160a3de';
+
+// function fetchCurrentWeather(city) {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`)
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         var currentTemp = document.createElement("h4")
+//         currentTemp.innerHTML = `Temp: ${data.main.temp}  &#176F`
+//         currentCard.append(currentTemp)
+//     });
+// };
+
+// searchBtn.addEventListener("click", function() {
+//     var city = searchBar.value;
+//     localStorage.setItem("city", JSON.stringify(city));
+//     fetchCurrentWeather(city);
+// });
 
